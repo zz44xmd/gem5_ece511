31960f69e4c1efe2a9bbd02369444bb5b18f4166
zhengyang <zz44@illinois.edu>
2024-05-06 19:58:59 -0700

    memcpy_fedex

--- benchmarks/RISCV/memcpy_fedex.c
+++ benchmarks/RISCV/memcpy_fedex.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <stdint.h>
+#include <stdlib.h>
 
-#define DIM 1025
+#define DIM 10
+#define SHIFT 4
+#define SIZE (64L << SHIFT)
+#define ALIGN (4L << SHIFT)
 
-#define SIZE (64L << 20)
-#define ALIGN (4L << 20)
 
 #define MEMCPY(src, dest, len) \
     asm volatile ("memcpy %0, %1, %2" : : "r" (src), "r" (dest), "r" (len));
@@ -27,38 +30,48 @@ void matrix_multiply(int dim) {
     }
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        printf("Usage: %s <size> <dim>\n", argv[0]);
-        return 1;
-    }
+int main() {
+    // if (argc != 3) {
+    //     printf("Usage: %s <size> <dim>\n", argv[0]);
+    //     return 1;
+    // }
 
-    int size = atoi(argv[1]);
-    int dim = atoi(argv[2]);
+    int size = 5;
+    int dim = DIM;
+    double sec;
+
 
     char *src_p = (char *)(((unsigned long)buf_src + (ALIGN - 1)) & (~(ALIGN - 1)));
     char *dst_p = (char *)(((unsigned long)buf_dst + (ALIGN - 1)) & (~(ALIGN - 1)));
 
+    printf("%x\n", src_p);
+
     for (unsigned long i = 0; i < size; ++i) {
         src_p[i] = i % 64 + 32;
+        printf("%d\n", i);
+        
     }
 
     for (unsigned i = 0; i < DIM; ++i) {
         for (unsigned j = 0; j < DIM; ++j) {
             a[i][j] = 2 * i + j + 1;
             b[i][j] = i + 2 * j - 1;
+            printf("%d, %d\n", i, j);
         }
     }
-
+    clock_t start = clock();
+    // MEMCPY(dest_ptr, dest_ptr, len);
     MEMCPY(src_p, dst_p, size);
+
 
     /******* Some Other calculations *******/
     matrix_multiply(dim);
     /***************************************/
-
+    sec = (clock() - start) / (double)CLOCKS_PER_SEC;
+    printf("%15.1f \n", sec*1000000000.0);
     if (memcmp((const void*)src_p, (const void*)dst_p, size) != 0) {
         printf("[[FAILED]]\n");
-        return -1;
+        // return -1;
     }
     printf("[[PASSED]]\n");
     printf("src: %s\n", src_p);
